14693ac64a4975ddf9cd1a4164e04f53a842a158
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-08-29 11:52:46 +0900

    他にも設定を追加

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,14 @@
 <?php
 
-declare(strict_types=1);
-
-$finder = PhpCsFixer\Finder::create()->in(__DIR__);
-
-return (new PhpCsFixer\Config())
-    ->setRules([
-        'declare_strict_types' => true,
-    ])
-    ->setFinder($finder);
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__);
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    'declare_strict_types' => true, // declare(strict_types = 1);を追加
+    'declare_equal_normalize' => ['space' => 'single'], // declare文の等号の前後にシングルスペースを入れる
+    'explicit_string_variable' => true,  // 文字列演算の際に明示的な変数を使う
+    'escape_implicit_backslashes' => true, // 文字列内のバックスラッシュをエスケープする
+])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setLineEnding("\n");
