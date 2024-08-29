de13ffcc97d800f1167a7f0f75ac4a6b137c1e5f
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-08-29 12:03:06 +0900

    single_blank_line_at_eofを追加

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,10 +4,11 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__);
 $config = new PhpCsFixer\Config();
 return $config->setRules([
-    'declare_strict_types' => true, // declare(strict_types = 1);を追加
+    'declare_strict_types' => true,                     // declare(strict_types = 1);を追加
     'declare_equal_normalize' => ['space' => 'single'], // declare文の等号の前後にシングルスペースを入れる
-    'explicit_string_variable' => true,  // 文字列演算の際に明示的な変数を使う
-    'escape_implicit_backslashes' => true, // 文字列内のバックスラッシュをエスケープする
+    'explicit_string_variable' => true,                 // 文字列演算の際に明示的な変数を使う
+    'escape_implicit_backslashes' => true,              // 文字列内のバックスラッシュをエスケープする
+    'single_blank_line_at_eof' => true,                 // ファイルの最後に1行の空行を追加 (gitのdiff問題を回避)
 ])
     ->setFinder($finder)
     ->setRiskyAllowed(true)
